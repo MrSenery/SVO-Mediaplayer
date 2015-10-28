875b9b4f27b597bf478e2587d24e66e93e01e02d
MrSenery <sandervanosch@msn.com>
2015-10-28 15:48:22 +0100

    Fix alot of stuff, added list for song name and artist, added selectable mp3 and is playeable

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -28,13 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.VolumeBar = new System.Windows.Forms.TrackBar();
             this.TimeBar = new System.Windows.Forms.TrackBar();
             this.FolderButton = new System.Windows.Forms.Button();
             this.FileButton = new System.Windows.Forms.Button();
-            this.InfoTextBox = new System.Windows.Forms.RichTextBox();
             this.PauseButton = new System.Windows.Forms.Button();
             this.PlayButton = new System.Windows.Forms.Button();
+            this.MusicTimer = new System.Windows.Forms.Timer(this.components);
+            this.MusicListBox = new System.Windows.Forms.ListView();
+            this.ColName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColArtist = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColAlbum = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColLength = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             ((System.ComponentModel.ISupportInitialize)(this.VolumeBar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.TimeBar)).BeginInit();
             this.SuspendLayout();
@@ -42,7 +48,7 @@
             // VolumeBar
             // 
             this.VolumeBar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.VolumeBar.Location = new System.Drawing.Point(302, 74);
+            this.VolumeBar.Location = new System.Drawing.Point(383, 45);
             this.VolumeBar.Name = "VolumeBar";
             this.VolumeBar.Orientation = System.Windows.Forms.Orientation.Vertical;
             this.VolumeBar.Size = new System.Drawing.Size(45, 146);
@@ -61,7 +67,7 @@
             // FolderButton
             // 
             this.FolderButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.FolderButton.Location = new System.Drawing.Point(302, 226);
+            this.FolderButton.Location = new System.Drawing.Point(383, 197);
             this.FolderButton.Name = "FolderButton";
             this.FolderButton.Size = new System.Drawing.Size(75, 23);
             this.FolderButton.TabIndex = 11;
@@ -77,17 +83,7 @@
             this.FileButton.TabIndex = 10;
             this.FileButton.Text = "File";
             this.FileButton.UseVisualStyleBackColor = true;
-            // 
-            // InfoTextBox
-            // 
-            this.InfoTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.InfoTextBox.Location = new System.Drawing.Point(12, 74);
-            this.InfoTextBox.Name = "InfoTextBox";
-            this.InfoTextBox.Size = new System.Drawing.Size(284, 175);
-            this.InfoTextBox.TabIndex = 9;
-            this.InfoTextBox.Text = "";
+            this.FileButton.Click += new System.EventHandler(this.FileButton_Click);
             // 
             // PauseButton
             // 
@@ -109,16 +105,51 @@
             this.PlayButton.UseVisualStyleBackColor = true;
             this.PlayButton.Click += new System.EventHandler(this.PlayButton_Click);
             // 
+            // MusicListBox
+            // 
+            this.MusicListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.MusicListBox.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ColName,
+            this.ColArtist,
+            this.ColAlbum,
+            this.ColLength});
+            this.MusicListBox.Location = new System.Drawing.Point(12, 74);
+            this.MusicListBox.Name = "MusicListBox";
+            this.MusicListBox.Size = new System.Drawing.Size(365, 175);
+            this.MusicListBox.TabIndex = 14;
+            this.MusicListBox.UseCompatibleStateImageBehavior = false;
+            this.MusicListBox.View = System.Windows.Forms.View.Details;
+            // 
+            // ColName
+            // 
+            this.ColName.Text = "Name";
+            this.ColName.Width = 120;
+            // 
+            // ColArtist
+            // 
+            this.ColArtist.Text = "Artist";
+            this.ColArtist.Width = 80;
+            // 
+            // ColAlbum
+            // 
+            this.ColAlbum.Text = "Album";
+            this.ColAlbum.Width = 85;
+            // 
+            // ColLength
+            // 
+            this.ColLength.Text = "Length";
+            this.ColLength.Width = 76;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(470, 261);
+            this.Controls.Add(this.MusicListBox);
             this.Controls.Add(this.VolumeBar);
             this.Controls.Add(this.TimeBar);
             this.Controls.Add(this.FolderButton);
             this.Controls.Add(this.FileButton);
-            this.Controls.Add(this.InfoTextBox);
             this.Controls.Add(this.PauseButton);
             this.Controls.Add(this.PlayButton);
             this.Name = "Main";
@@ -136,9 +167,14 @@
         private System.Windows.Forms.TrackBar TimeBar;
         private System.Windows.Forms.Button FolderButton;
         private System.Windows.Forms.Button FileButton;
-        private System.Windows.Forms.RichTextBox InfoTextBox;
         private System.Windows.Forms.Button PauseButton;
         private System.Windows.Forms.Button PlayButton;
+        private System.Windows.Forms.Timer MusicTimer;
+        private System.Windows.Forms.ListView MusicListBox;
+        private System.Windows.Forms.ColumnHeader ColName;
+        private System.Windows.Forms.ColumnHeader ColArtist;
+        private System.Windows.Forms.ColumnHeader ColAlbum;
+        private System.Windows.Forms.ColumnHeader ColLength;
 
     }
 }
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -8,50 +8,41 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Media;
-using CSCore;
-using CSCore.Codecs;
-using CSCore.CoreAudioAPI;
-using CSCore.SoundOut;
+using NAudio;
+using NAudio.Wave;
+using TagLib;
 
 namespace WindowsFormsApplication1
 {
+
     public partial class Main : Form
     {
-        public IWaveSource soundSource;
-        public ISoundOut soundOut;
-        bool Played = true;
+        IWavePlayer Player = new WaveOut();
+        OpenFileDialog Mp3File = new OpenFileDialog();
+        List<string> Items = new List<string>();
         public Main()
         {
             InitializeComponent();
+            Mp3File.FileName = "";
         }
 
         private void PlayButton_Click(object sender, EventArgs e)
         {
-            soundSource = GetSoundSource();
-            soundOut = GetSoundOut();
-            soundOut.Initialize(soundSource);
-            //if (Played)
-            //{
-                soundOut.Play();
-            //    Played = false;
-            //}
-            //else
-            //{
-            //    soundOut.Resume();
-            //}
+            try
+            {
+                AudioFileReader File = new AudioFileReader(Mp3File.FileName);
+                Player.Init(File);
+                Player.Play();
+            }
+            catch (Exception ex)
+            {
+                
+            }
         }
-        private ISoundOut GetSoundOut()
-        {
-            return new WasapiOut();
-        }
-        private IWaveSource GetSoundSource()
-        {
-            return CodecFactory.Instance.GetCodec(@"C:/Users/Admin/Desktop/Spit It Out.mp3");
-        }
-
         private void PauseButton_Click(object sender, EventArgs e)
         {
-            soundOut.Stop();
+            Player.Pause();
+            Player.Dispose();
         }
 
         private void Main_FormClosing(object sender, FormClosingEventArgs e)
@@ -59,9 +50,25 @@ namespace WindowsFormsApplication1
             Application.Exit();
         }
 
-        private void Main_FormClosing(object sender, FormClosedEventArgs e)
+        private void FileButton_Click(object sender, EventArgs e)
         {
-
+            Mp3File.Title = "Choose a .mp3 file";
+            Mp3File.Filter = "Mp3 Files|*.mp3";
+            Mp3File.InitialDirectory = "C:/";
+            if (Mp3File.ShowDialog() == DialogResult.OK)
+            {
+                TagLib.File TagFile = TagLib.File.Create(Mp3File.FileName);
+                string Name = TagFile.Tag.Title;
+                string Artist = TagFile.Tag.FirstPerformer;
+                //var Items = MusicListBox.Items;
+                //MusicListBox.Items.Add(Name);
+                ////ListViewItem Items = new ListViewItem(MusicListBox);
+                //MusicListBox.Items.Add(Artist);
+                ListViewItem item = new ListViewItem();
+                item.Text = Name;
+                item.SubItems.Add(Artist);
+                MusicListBox.Items.Add(item);
+            }
         }
     }
 }
